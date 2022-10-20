0b32013d4c4b528b1e2120a5f9806e12af351a91
Madi <abaymadi1@gmail.com>
2022-10-20 13:23:09 +0600

    first commit

---
+++ news/templatetags/news_tags.py
@@ -0,0 +1,23 @@
+from django import template
+from django.db.models import Count, F
+from django.core.cache import cache
+
+from news.models import Category
+
+register = template.Library()
+
+@register.simple_tag()
+def get_categories():
+    return Category.objects.all()
+
+@register.inclusion_tag('news/list_categories.html')
+def show_categories():
+    # categories = Category.objects.all()
+
+    # categories = cache.get('categories')
+    # if not categories:
+    #     categories = Category.objects.annotate(cnt=Count('news', filter=(F('news__is_published')))).filter(cnt__gt=0)
+    #     cache.set('categories', categories, 30)
+
+    categories = Category.objects.annotate(cnt=Count('news', filter=(F('news__is_published')))).filter(cnt__gt=0)
+    return {'categories': categories}
